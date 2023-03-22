dd8fac227f5956cc6347c075c3c65f6f2e155802
clever-cr <cleverumuhuza@gmail.com>
2023-03-22 11:12:12 +0200

    refactor codes

--- src/App.js
+++ src/App.js
@@ -3,45 +3,45 @@ import { useState } from 'react';
 
 
 const App = () => {
+  const values = ["AC", "+/-", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="]
 
   const [displayValue, setDisplayValue] = useState("")
+
   const [operation, setOperation] = useState({ operand1: "", operator: null, operand2: "" })
+
   const [results, setResults] = useState("")
 
-  console.log("results", results)
-  console.log("operation", operation)
   const selectOperator = (operator) => {
     setOperation(prevOperation => ({ ...prevOperation, operator: operator }))
     setDisplayValue("")
   }
 
-  const selectOperand1 = (operand1) => {
-    setOperation(prevOperation => ({ ...prevOperation, operand1: prevOperation.operand1 + operand1 }))
-  }
+  const selectOperand1 = (operand1) => { setOperation(prevOperation => ({ ...prevOperation, operand1: prevOperation.operand1 + operand1 })) }
 
-  const selectOperand2 = (operand2) => {
-    setOperation(prevOperation => ({ ...prevOperation, operand2: prevOperation.operand2 + operand2 }))
-  }
+  const selectOperand2 = (operand2) => { setOperation(prevOperation => ({ ...prevOperation, operand2: prevOperation.operand2 + operand2 })) }
 
-  const handleChange = (newNumber) => {
-    setDisplayValue(prevValue => prevValue + newNumber)
+  const handleChange = (newNumber) => { setDisplayValue(prevValue => prevValue + newNumber) }
+
+  const handleCalculate = () => { return calculate(+operation.operand1, +operation.operand2, operation.operator) }
+
+  const clear = () => {
+    setResults("")
+    setDisplayValue("")
   }
 
   const handleClick = (event) => {
     if (event.target.value === "=") {
-      setResults(calculate(+operation.operand1, +operation.operand2, operation.operator))
-      setDisplayValue(calculate(+operation.operand1, +operation.operand2, operation.operator))
+      setResults(handleCalculate)
+      setDisplayValue(handleCalculate)
     }
 
-    else if (["+", "-", "*", "/"].includes(event.target.value)) {
-      selectOperator(event.target.value)
-    }
+    else if (["+", "-", "*", "/"].includes(event.target.value)) selectOperator(event.target.value)
+
 
     else if (event.target.value === 'AC') {
       if (results !== "") {
-        setResults("");
+        clear()
         setOperation({ operand1: "", operator: null, operand2: "" });
-        setDisplayValue("")
       }
       else if (operation.operand1 && operation.operator) {
         setOperation(prevOperation => ({ ...prevOperation, operand2: "" }))
@@ -76,16 +76,16 @@ const App = () => {
         setDisplayValue(operation.operand1 * -1)
       }
     }
+
     else if (event.target.value === '0') {
       if (results !== "") {
-        setResults("")
+        clear()
         setOperation({ operand1: "", operator: null, operand2: "" })
         selectOperand1(event.target.value)
-        setDisplayValue("")
+
       }
-      else if (operation.operand1 === "") {
-        setDisplayValue("")
-      }
+      else if (operation.operand1 === "") setDisplayValue("")
+
       else {
         if (operation.operand2) {
           setOperation((prevOperation => ({ ...prevOperation, operand2: prevOperation.operand2 + event.target.value })))
@@ -97,21 +97,18 @@ const App = () => {
         }
       }
     }
+
     else {
       if (results !== "") {
-        setResults("")
+        clear()
         setOperation({ operand1: "", operator: null, operand2: "" })
         selectOperand1(event.target.value)
-        setDisplayValue("")
-      }
-      else if (operation.operand1 && operation.operator) {
-        selectOperand2(event.target.value)
 
       }
-      else {
-        selectOperand1(event.target.value)
+      else if (operation.operand1 && operation.operator) selectOperand2(event.target.value)
 
-      }
+      else selectOperand1(event.target.value)
+
       handleChange(event.target.value)
     }
   }
@@ -131,33 +128,16 @@ const App = () => {
     }
   }
 
-
   return (
     <div className="w-[300px] m-32">
       <input onChange={handleChange} placeholder="" value={displayValue} name="number" className="w-[300px] outline-none bg-gray-600 p-5 text-right placeholder:text-right placeholder:text-white  text-white" />
-      <div className="grid grid-cols-4 gap-[3px]-">
-        <Button value='AC' handleClick={handleClick} />
-        <Button value='+/-' handleClick={handleClick} />
-        <Button value='%' handleClick={handleClick} />
-        <Button value='/' style={`${"bg-orange-500"}`} handleClick={handleClick} />
-        <Button value='7' handleClick={handleClick} />
-        <Button value='8' handleClick={handleClick} />
-        <Button value='9' name='number' handleClick={handleClick} />
-        <Button value='*' style={`${"bg-orange-500"}`} handleClick={handleClick} />
-        <Button value='4' handleClick={handleClick} />
-        <Button value='5' handleClick={handleClick} />
-        <Button value='6' handleClick={handleClick} />
-        <Button value='-' style={`${"bg-orange-500"}`} handleClick={handleClick} />
-        <Button value='1' handleClick={handleClick} />
-        <Button value='2' handleClick={handleClick} />
-        <Button value='3' handleClick={handleClick} />
-        <Button value='+' style={`${"bg-orange-500"}`} handleClick={handleClick} />
-        <Button style={`${'col-span-2'}`} value='0' handleClick={handleClick} />
-        <Button value='.' handleClick={handleClick} />
-        <Button value='=' style={`${"bg-orange-500"}`} handleClick={handleClick} />
-
+      <div className="grid grid-cols-4">
+        {values.map((item, index) => {
+          return (
+            <Button key={index} value={item} handleClick={handleClick} style={`${index === 3 || index === 7 || index === 11 || index === 15 || index === 18 ? "bg-orange-500" : ""} ${index === 16 ? "col-span-2" : ""}`} />
+          )
+        })}
       </div>
-
     </div>
   )
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Button = ({ value, style, handleClick, name }) => {
     return (
         <button name={name} value={value} onClick={handleClick} className={`bg-gray-200 p-5 border border-gray-50 cursor-pointer ${style}`}>{value}</button>
